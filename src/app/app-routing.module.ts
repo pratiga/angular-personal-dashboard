efb61a2c3de7af3dd86bdea76379fe771fc9f315
pratiga <gaitrisanu2468@gmail.com>
2022-08-13 21:18:35 +0545

    add route
    Signed-off-by: pratiga <gaitrisanu2468@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { NotesComponent } from './notes/notes.component';
+import { TodosComponent } from './todos/todos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'bookmarks', component: BookmarksComponent},
+  {path: 'notes', component: NotesComponent},
+  {path: 'todos', component: TodosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
