50a9385777c1f9d98cd55b065e8148813a42b984
pratiga <gaitrisanu2468@gmail.com>
2022-08-15 23:01:24 +0545

    add more modules
    Signed-off-by: pratiga <gaitrisanu2468@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TabsComponent } from './tabs/tabs.component';
 import { BookmarksComponent } from './bookmarks/bookmarks.component';
 import { NotesComponent } from './notes/notes.component';
 import { TodosComponent } from './todos/todos.component';
+import { AddNoteComponent } from './add-note/add-note.component';
+import { NoteCardComponent } from './note-card/note-card.component';
+import { EditNoteComponent } from './edit-note/edit-note.component';
 
 @NgModule({
   declarations: [
@@ -14,12 +18,17 @@ import { TodosComponent } from './todos/todos.component';
     TabsComponent,
     BookmarksComponent,
     NotesComponent,
-    TodosComponent
+    TodosComponent,
+    AddNoteComponent,
+    NoteCardComponent,
+    EditNoteComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
